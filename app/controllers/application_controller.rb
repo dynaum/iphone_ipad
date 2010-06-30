da9e92cba7da22c3deb71db9b9f12bfa95172017
Elber Ribeiro <dynaumloko@gmail.com>
2010-06-30 00:07:00 -0300

    alteração no filtro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
+  
+  before_filter :set_iphone_or_ipad_format
+
+  def set_iphone_or_ipad_format
+    format = is_iphone_or_ipad_request?
+    unless format.nil?
+      request.format = format
+    end
+  end
+  
+  def is_iphone_or_ipad_request?
+    request.user_agent =~ /(iPhone|iPad).+Mobile\/.+Safari/
+    $1.downcase if $1
+  end
 end
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,6 @@
+class HomeController < ApplicationController
+  respond_to :html, :iphone, :ipad
+  
+  def index
+  end
+end
